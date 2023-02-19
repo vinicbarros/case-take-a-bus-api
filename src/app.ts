a4cb16370e67429439f6e01b442a86582d446e0f
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-19 11:15:59 -0300

    feat: created location router

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import "express-async-errors";
 import express, { Express, json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { locationRouter } from "@/router";
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use("/location", locationRouter);
 
 export async function init(): Promise<Express> {
   return Promise.resolve(app);
