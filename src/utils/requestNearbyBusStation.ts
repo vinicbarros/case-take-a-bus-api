5d235e1852c0ce797780081b95419bab16eb5f60
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-20 12:06:28 -0300

    feat: created a request function to get the nearby bus station

---
+++ src/utils/requestNearbyBusStation.ts
@@ -0,0 +1,19 @@
+import { badRequestError } from "@/error";
+import { ILocationParams, ResultNearbyStationType } from "@/types";
+import axios from "axios";
+
+export async function requestNearbyBusStation({
+  latitude,
+  longitude,
+}: ILocationParams): Promise<ResultNearbyStationType[]> {
+  const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+  const searchUrl = `${url}location=${latitude},${longitude}&radius=500&keyboard=bus+station&key=${process.env.GOOGLE_API_KEY}`;
+
+  const result = await axios.get(searchUrl);
+
+  if (result.data.status === "INVALID_REQUEST") {
+    throw badRequestError();
+  }
+  return result.data.results as ResultNearbyStationType[];
+}
