d6ed99cde58adfcdaf6450ecee371ec8d04a2e89
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-20 12:06:54 -0300

    feat: created location types

---
+++ src/types/location-types.ts
@@ -0,0 +1,40 @@
+export interface ILocationParams {
+  latitude: string;
+  longitude: string;
+}
+
+export type ResultNearbyStationType = {
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+    viewport: {
+      northeast: {
+        lat: number;
+        lng: number;
+      };
+      southwest: {
+        lat: number;
+        lng: number;
+      };
+    };
+  };
+  icon: string;
+  icon_background_color: string;
+  icon_mask_base_uri: string;
+  name: string;
+  photos: [
+    {
+      height: number;
+      html_attributions: [string];
+      photo_reference: string;
+      width: number;
+    }
+  ];
+  place_id: string;
+  reference: string;
+  scope: string;
+  types: string[];
+  vicinity: string;
+};
