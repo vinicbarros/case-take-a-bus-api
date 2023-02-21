d92d867c403af0f40c97e6012effc6aa0b89986d
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-02-21 18:20:30 -0300

    feat: implemented a validation middleware in location router

--- src/router/location-router.ts
+++ src/router/location-router.ts
@@ -1,8 +1,10 @@
 import { nearbyBusStation } from "@/controller";
+import { validateBody } from "@/middleware";
+import { locationBodySchema } from "@/schema/location-schema";
 import { Router } from "express";
 
 const locationRouter = Router();
 
-locationRouter.post("/", nearbyBusStation);
+locationRouter.post("/", validateBody(locationBodySchema), nearbyBusStation);
 
 export { locationRouter };
